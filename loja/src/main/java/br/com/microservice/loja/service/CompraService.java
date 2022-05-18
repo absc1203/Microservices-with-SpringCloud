2f9b32bd401b2ea6910efba50541f525748c5c6a
Andre <absc1203@gmail.com>
2022-05-18 17:54:35 -0300

    Implementando o service discovery Eureka

--- loja/src/main/java/br/com/microservice/loja/service/CompraService.java
+++ loja/src/main/java/br/com/microservice/loja/service/CompraService.java
@@ -1,5 +1,6 @@
 package br.com.microservice.loja.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -11,13 +12,15 @@ import br.com.microservice.loja.controller.dto.InfoFornecedorDTO;
 @Service
 public class CompraService {
 
+	@Autowired
+	private RestTemplate client;
+
 	public void realizaCompra(CompraDTO compra) {
 
-		RestTemplate client = new RestTemplate();
 		ResponseEntity<InfoFornecedorDTO> exchange = client.exchange(
-				"http://localhost:8081/info/" + compra.getEndereco().getEstado(), HttpMethod.GET, null,
+				"http://fornecedor/info/" + compra.getEndereco().getEstado(), HttpMethod.GET, null,
 				InfoFornecedorDTO.class);
-		
+
 		System.out.println(exchange.getBody().getEndereco());
 
 	}
